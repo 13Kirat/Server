fce3f75b82eaac55f21dddda38753bb054795cde
13Kirat <141816179+13Kirat@users.noreply.github.com>
2025-03-26 20:23:06 +0530

    added username field

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,13 +2,13 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 exports.registerUser = async (req, res) => {
-  const { email, password, isAdmin } = req.body;
+  const { username, email, password, isAdmin } = req.body;
 
   try {
     let user = await User.findOne({ email });
     if (user) return res.status(400).json({ msg: 'User already exists' });
 
-    user = new User({ email, password, isAdmin });
+    user = new User({ username, email, password, isAdmin });
 
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(password, salt);
@@ -19,7 +19,7 @@ exports.registerUser = async (req, res) => {
     console.error(err.message);
     res.status(500).send('Server error');
   }
-};  
+};
 
 // Fetch all users (Admin only)
 exports.getAllUsers = async (req, res) => {
